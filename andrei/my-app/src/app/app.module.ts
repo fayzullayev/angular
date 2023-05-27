4d329aad084bac500b5e8bf51322bb3d1e0ca9ec
Mirodil <odilfayz22@gmail.com>
2023-05-27 18:49:30 +0500

    .

--- andrei/my-app/src/app/app.module.ts
+++ andrei/my-app/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OoComponent } from './oo/oo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, OoComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ andrei/my-app/src/app/oo/oo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-oo',
+  templateUrl: './oo.component.html',
+  styleUrls: ['./oo.component.css']
+})
+export class OoComponent {
+
+}
