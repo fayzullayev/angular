e6d477f47632d87619663598d9c95f239b293a7d
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-05-29 14:30:26 +0500

    .

--- andrei/my-app/src/app/home-page/home-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-root-app',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css'],
-})
-export class HomePageComponent {}
