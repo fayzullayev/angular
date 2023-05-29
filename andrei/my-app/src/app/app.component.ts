95dc3c7292e300c8877c2e4fe53bad1f61967cbf
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-05-29 16:57:20 +0500

    .

--- andrei/my-app/src/app/app.component.ts
+++ andrei/my-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public name: string = 'John';
+  public name: string = 'Dwayne John';
 }
