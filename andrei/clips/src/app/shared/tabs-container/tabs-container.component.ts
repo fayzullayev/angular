1cfba205a54b20f19a7629be88011fa8f4c752b5
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-06-08 20:09:08 +0500

    .

--- andrei/clips/src/app/shared/tabs-container/tabs-container.component.ts
+++ andrei/clips/src/app/shared/tabs-container/tabs-container.component.ts
@@ -3,6 +3,7 @@ import {
   Component,
   ContentChildren,
   OnInit,
+  QueryList,
 } from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 
@@ -12,11 +13,26 @@ import { TabComponent } from '../tab/tab.component';
   styleUrls: ['./tabs-container.component.css'],
 })
 export class TabsContainerComponent implements OnInit, AfterContentInit {
-  @ContentChildren(TabComponent) tabs = {};
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent> =
+    new QueryList<TabComponent>();
 
   ngOnInit() {}
 
   ngAfterContentInit() {
+    // this.tabs.;
     console.log(this.tabs);
+    const activeTabs = this.tabs?.filter((item: TabComponent) => item.active);
+
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first);
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach((item) => {
+      item.active = false;
+    });
+
+    tab.active = true;
   }
 }
