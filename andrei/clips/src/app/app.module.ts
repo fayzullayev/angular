d39fcb000f50416a8ea25a3bd62381e671603bb1
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-07-03 13:51:21 +0500

    after pause

--- andrei/clips/src/app/app.module.ts
+++ andrei/clips/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
-
 import { NavComponent } from './nav/nav.component';
 
 @NgModule({
