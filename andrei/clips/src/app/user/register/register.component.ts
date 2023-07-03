9893acce72fb289ea0ca3d10c57082dc073cbde1
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-07-03 15:17:39 +0500

    .

--- andrei/clips/src/app/user/register/register.component.ts
+++ andrei/clips/src/app/user/register/register.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-
+  registerForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', Validators.email),
+    age: new FormControl(''),
+    password: new FormControl(''),
+    confirm_password: new FormControl(''),
+    phoneNumber: new FormControl(''),
+  });
 }
