f4ccf7624188133fe7a65cdc75bc48cfdad66505
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-08-09 18:18:08 +0500

    .

--- charts/charts_tutorial/src/app/app.component.ts
+++ charts/charts_tutorial/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import type { EChartsOption } from 'echarts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-  title = 'charts_tutorial';
+  chartOption: EChartsOption = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [
+          120,
+          {
+            value: 200,
+            itemStyle: {
+              color: 'green',
+            },
+          },
+          150,
+          80,
+          70,
+          110,
+          130,
+        ],
+        type: 'bar',
+      },
+    ],
+  };
 }
